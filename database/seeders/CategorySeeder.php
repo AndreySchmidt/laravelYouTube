ba340016c183118b2349983b22256816bae82055
AndreySchmidt <ringnull@gmail.com>
2023-03-18 16:53:30 +0300

    many-to-many

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,19 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Channel;
 use App\Models\Category;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 // use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     *  ./vendor/bin/sail artisan db:seed --class=CategorySeeder
      */
     public function run(): void
     {
-        Category::factory()->count(10)->create();
+        // Category::factory()->count(10)->create();
 
         // $arrCategoryList = [];
         // foreach( range(1, 3) AS $intI ){
@@ -28,5 +32,25 @@ class CategorySeeder extends Seeder
         // {
         //     DB::table('categories')->insert($arrCategoryList);
         // }
+
+        # many to many start
+        // создаем канал и пользователя (сразу линкуем к каналу) один-к-одному
+        // $objChannelItem = Channel::factory()->forUser()->create();
+        // создаем три видео  на канале (который создан ранее) один-ко-многим
+        // $objVideoList = Video::factory(3)->for($objChannelItem)->create();
+        // создаем категории и привязать к ним существующие видео (вставив коллекцию видео в метод hasAttached) многие-ко-многим
+        // Category::factory(3)->hasAttached($objVideoList)->create();
+        # many to many end
+
+
+        # inverse many to many start
+        // создаем канал и пользователя (сразу линкуем к каналу) один-к-одному
+        $objChannelItem = Channel::factory()->forUser()->create();
+        // создаем одно видео на канале (который создан ранее) один-ко-многим, тут только одно видео, но всё же ))
+        // создаю и линкую две категории с видео
+        // (hasCategories для работы метода требуется наличие метода categories в модели видео, который возвращает объект отношения)
+        $objVideoList = Video::factory()->for($objChannelItem)->hasCategories(2)->create();
+
+        # inverse many to many end
     }
 }
