f9af42d43b83aef52043a08ae79c6542e5c12b09
AndreySchmidt <ringnull@gmail.com>
2023-03-22 16:18:42 +0300

    videoList by period (get)

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -9,7 +9,67 @@ class VideoController extends Controller
 {
     public function index()
     {
-        return Video::with('channel', 'categories')->get();
+        // верну количество записей видео относительно времени их добавления
+        // return [
+        //     'year' => Video::with('channel', 'categories')
+        //     ->where('created_at', '>=', now()->startOfYear())
+        //     ->count(),
+        //     'month' => Video::with('channel', 'categories')
+        //     ->where('created_at', '>=', now()->startOfMonth())
+        //     ->count(),
+        //     'week' => Video::with('channel', 'categories')
+        //     ->where('created_at', '>=', now()->startOfWeek())
+        //     ->count(),
+        //     'day' => Video::with('channel', 'categories')
+        //     ->where('created_at', '>=', now()->startOfDay())
+        //     ->count(),
+        //     'hour' => Video::with('channel', 'categories')
+        //     ->where('created_at', '>=', now()->startOfHour())
+        //     ->count(),
+        // ];
+
+        // return Video::with('channel', 'categories')
+        //     ->where('created_at', '>=', now()->startOfYear())
+        //     ->get();
+
+        // верну данные по периоду (строка в гет параметрах)
+        // http://localhost/api/videos?period=year
+        // http://localhost/api/videos?period=week
+
+        // $strPeriod = request('period');
+        // if($strPeriod)
+        // {
+        //     $arrVideoList = [
+        //         'year' => Video::with('channel', 'categories')
+        //         ->where('created_at', '>=', now()->startOfYear())
+        //         ->get(),
+        //         'month' => Video::with('channel', 'categories')
+        //         ->where('created_at', '>=', now()->startOfMonth())
+        //         ->get(),
+        //         'week' => Video::with('channel', 'categories')
+        //         ->where('created_at', '>=', now()->startOfWeek())
+        //         ->get(),
+        //         'day' => Video::with('channel', 'categories')
+        //         ->where('created_at', '>=', now()->startOfDay())
+        //         ->get(),
+        //         'hour' => Video::with('channel', 'categories')
+        //         ->where('created_at', '>=', now()->startOfHour())
+        //         ->get(),
+        //     ];
+        //     return $arrVideoList[$strPeriod];
+        // }
+
+        switch (request('period'))
+        {
+            case 'year': $arrVideoList = Video::where('created_at', '>=', now()->startOfYear())->get(); break;
+            case 'month': $arrVideoList = Video::where('created_at', '>=', now()->startOfMonth())->get(); break;
+            case 'week': $arrVideoList = Video::where('created_at', '>=', now()->startOfWeek())->get(); break;
+            case 'day': $arrVideoList = Video::where('created_at', '>=', now()->startOfDay())->get(); break;
+            case 'hour': $arrVideoList = Video::where('created_at', '>=', now()->startOfHour())->get(); break;
+            default: $arrVideoList = Video::with('channel', 'categories')->get(); break;
+        }
+
+        return $arrVideoList;
     }
 
     public function show(Video $video)
--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -17,9 +17,20 @@ class VideoFactory extends Factory
      */
     public function definition(): array
     {
+        $strDate = $this->faker->dateTimeThisYear();
+        // $strDate = $this->faker->dateTimeThisMonth();
+        // $strDate = $this->faker->dayOfWeek();
+        // между 2 месяца назад от сейчас до 1 недели вперед
+        // $strDate = $this->faker->dateTimeBetween('-2 months', '+1 week');
+        // между 1 неделя назад от 1 недели назад до 3 дней вперед
+        // $strDate = $this->faker->dateTimeInInterval('-1 week', '+3 days');
+
         return [
             'title' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
             'channel_id' => Channel::inRandomOrder()->first()->id,
+            // сгенерирую даты фейкером
+            'created_at' => $strDate,
+            'updated_at' => $strDate,
         ];
     }
 }
