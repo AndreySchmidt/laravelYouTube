6d6cc66717f8e83d85978350c22699fbf16b6eb3
AndreySchmidt <ringnull@gmail.com>
2023-03-25 09:58:57 +0300

    refactoring video seeder & factory

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\Period;
 use App\Models\Channel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -10,29 +11,66 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class VideoFactory extends Factory
 {
+    public function definition(): array
+    {
+        // $createdAt = $this->createdAt();
+
+        return [
+            'title' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
+            'description' => $this->faker->sentences(3, true),
+            'channel_id' => Channel::inRandomOrder()->first(),
+            // сгенерирую даты фейкером (закоментил ибо буду менять это через стейт, вызов в сидере)
+            // 'created_at' => $createdAt,
+            // 'updated_at' => $createdAt,
+        ];
+    }
+
+    // public function createdAt()
+    // {
+    //     $period = $this->faker->randomElement(['year', 'month', 'week', 'day', 'hour']);
+    //     return $this->faker->dateTimeBetween("-1 $period");
+    // }
+
+    // в ларавел есть стандартные средства, например состояние фабрики (этот метод представляет состояние ласт)
+    public function last(Period $period)
+    {
+        
+        return $this->state(function() use ($period)
+        {
+            // $period->value получим в виде строки
+            $createdAt = $this->faker->dateTimeBetween("-1 $period->value");
+
+            // переопределим в стейте даты создания, обновления
+            return [
+                'created_at' => $createdAt,
+                'updated_at' => $createdAt,
+            ];
+        });
+    }
+
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        $strDate = $this->faker->dateTimeThisYear();
-        // $strDate = $this->faker->dateTimeThisMonth();
-        // $strDate = $this->faker->dayOfWeek();
-        // между 2 месяца назад от сейчас до 1 недели вперед
-        // $strDate = $this->faker->dateTimeBetween('-2 months', '+1 week');
-        // между 1 неделя назад от 1 недели назад до 3 дней вперед
-        // $strDate = $this->faker->dateTimeInInterval('-1 week', '+3 days');
+    // public function definition(): array
+    // {
+    //     $strDate = $this->faker->dateTimeThisYear();
+    //     // $strDate = $this->faker->dateTimeThisMonth();
+    //     // $strDate = $this->faker->dayOfWeek();
+    //     // между 2 месяца назад от сейчас до 1 недели вперед
+    //     // $strDate = $this->faker->dateTimeBetween('-2 months', '+1 week');
+    //     // между 1 неделя назад от 1 недели назад до 3 дней вперед
+    //     // $strDate = $this->faker->dateTimeInInterval('-1 week', '+3 days');
 
-        return [
-            'title' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
-            // true если одной строкой 4 предложения false - в виде массива
-            'description' => $this->faker->sentences(4, true),
-            'channel_id' => Channel::inRandomOrder()->first()->id,
-            // сгенерирую даты фейкером
-            'created_at' => $strDate,
-            'updated_at' => $strDate,
-        ];
-    }
+    //     return [
+    //         'title' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
+    //         // true если одной строкой 4 предложения false - в виде массива
+    //         'description' => $this->faker->sentences(4, true),
+    //         'channel_id' => Channel::inRandomOrder()->first()->id,
+    //         // сгенерирую даты фейкером
+    //         'created_at' => $strDate,
+    //         'updated_at' => $strDate,
+    //     ];
+    // }
 }
--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Period;
 use App\Models\Video;
 use App\Models\Channel;
 use Illuminate\Database\Seeder;
@@ -16,6 +17,24 @@ class VideoSeeder extends Seeder
      */
     public function run(): void
     {
+        // last() метод в фабрике для работы с состоянием
+        // Video::factory(4)->last('year')->create();
+        // Video::factory(5)->last('month')->create();
+        // Video::factory(2)->last('week')->create();
+        // Video::factory(3)->last('day')->create();
+        // Video::factory(1)->last('hour')->create();
+
+        // Video::factory(4)->last(Period::Year)->create();
+        // Video::factory(5)->last(Period::Month)->create();
+        // Video::factory(2)->last(Period::Week)->create();
+        // Video::factory(3)->last(Period::Day)->create();
+        // Video::factory(1)->last(Period::Hour)->create();
+
+        // на перечислениях есть стандартный метод cases() для получения всех значений свойств
+        // получу свойства и сделаю из них коллекцию
+        collect(Period::cases())->each(fn (Period $period) => Video::factory(4)->last($period)->create());
+
+
         // Video::factory()->count(10)->create();
 
         // $arrVideoList = [];
@@ -41,6 +60,6 @@ class VideoSeeder extends Seeder
 
         // создаем 2 видео и указываем какому каналу они должны принадлежать
         // (создав его, пользователь к каналу создается)
-        Video::factory()->count(2)->for(Channel::factory()->forUser())->create();
+        // Video::factory()->count(2)->for(Channel::factory()->forUser())->create();
     }
 }
