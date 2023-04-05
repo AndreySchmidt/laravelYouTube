68977952e3c41176699952d2afadca00041aa68f
AndreySchmidt <ringnull@gmail.com>
2023-04-05 19:31:09 +0300

    CommentSeeder refactoring

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -9,29 +9,34 @@ use Illuminate\Database\Seeder;
 class CommentSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * sudo ./vendor/bin/sail artisan db:seed --class=CommentSeeder
      */
     public function run(): void
     {
-        // Comment::factory(5)->create();
-        Video::take(3)->get()->flatMap(function (Video $video) {
-            $comments = Comment::factory(3)->create();
-            $video->comments()->saveMany($comments);
-
-            // return $comments->pluck('id')->all();
-            return $comments;
-        })->each(function (Comment $comment){
-            // чтобы не было вложенных чейнингов с комментами (оставлю 2 уровня)
-            if($comment->replies->isNotEmpty()) return;
-
-            $comment->parent()->associate(static::findRandomCommentToBeParent($comment))->save();
-        });
+        Video::take(3)->get()
+            ->flatMap(fn (Video $video) => static::seedCommentsFor($video))
+            ->each(fn (Comment $comment) => static::associateParentCommentWith($comment));
     }
 
-    public static function findRandomCommentToBeParent(Comment $comment)
+    private static function findRandomCommentToBeParent(Comment $comment)
     {
         return $comment->video->comments()->doesntHave('parent')
             ->where('id', '<>', $comment->id)
             ->inRandomOrder()->first();
+    }
+
+    private static function seedCommentsFor(Video $video)
+    {
+        $comments = Comment::factory(3)->create();
+        $video->comments()->saveMany($comments);
+        return $comments;
+    }
+
+    // присвоим комменту родителя
+    private static function associateParentCommentWith(Comment $comment)
+    {
+        // чтобы не было вложенных чейнингов с комментами (оставлю 2 уровня)
+        if($comment->replies->isNotEmpty()) return;
+        $comment->parent()->associate(static::findRandomCommentToBeParent($comment))->save();
     }
 }
