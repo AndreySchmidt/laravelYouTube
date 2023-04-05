a7af31a59f7b686d4395316383c4f61bf2ff390e
AndreySchmidt <ringnull@gmail.com>
2023-04-05 09:24:23 +0300

    migration comment

--- database/migrations/2023_04_05_045847_create_comments_table.php
+++ database/migrations/2023_04_05_045847_create_comments_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('text');
+            // id родительского коммента
+            $table->foreignId('parent_id')->nullable()->references('id')->on('comments');
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Video::class)->constrained();
             $table->timestamps();
         });
     }
