565964ae6722857464a32138ecb9296401969dec
AndreySchmidt <ringnull@gmail.com>
2023-04-12 13:04:00 +0300

    register

---
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterUserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'unique'.User::class, 'email', 'max:255'],
+            'password' => ['required', 'string', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::post('/register', dd(request()));
+
+Route::post('/register', [App\Http\Controllers\Auth\RegisterUserController::class, 'store']);
