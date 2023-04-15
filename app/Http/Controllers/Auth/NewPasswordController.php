8bbaee64fc1243567cdcf25fca31b0f91282cc64
AndreySchmidt <ringnull@gmail.com>
2023-04-15 15:00:32 +0300

    pass reset

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -2,13 +2,40 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
 
 class NewPasswordController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
+        $request->validate([
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
 
+        $status = Password::reset(
+            $request->only('token', 'email', 'password', 'password_confirmation'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => $request->password,
+                    'remember_token' => Str::random(60),
+                ])->save();
+            }
+        );
+
+        if($status != Password::PASSWORD_RESET)
+        {
+            throw ValidationException::withMessages([
+                'email' => [__($status)]
+            ]);
+        }
+
+        return response(['status' => __($status)]);
     }
 }
